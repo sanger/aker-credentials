c715d2a30bb2f28e6e5618741083f6aa8b0a77ac
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-01-03 13:33:45 +0000

    Use JWT NBF time as constant

--- lib/jwt_serializer.rb
+++ lib/jwt_serializer.rb
@@ -4,9 +4,9 @@ require 'ostruct'
 require 'faraday'
 
 class JWTSerializer < Faraday::Middleware
+  JWT_NBF_TIME = 60
 
   def call(env)
-    #debugger
     user_info = RequestStore.store[:x_authorisation]
     if user_info
       token = JWTSerializer.generate_jwt(user_info)
@@ -18,7 +18,6 @@ class JWTSerializer < Faraday::Middleware
   end
 
   def self.generate_jwt(auth_hash)
-    #debugger
     if auth_hash.is_a? OpenStruct
       auth_hash = auth_hash.to_h
     elsif !auth_hash.is_a? Hash
@@ -26,7 +25,7 @@ class JWTSerializer < Faraday::Middleware
     end
     secret_key = Rails.application.config.jwt_secret_key
     exp = Time.now.to_i + Rails.application.config.jwt_exp_time
-    nbf = Time.now.to_i - Rails.application.config.jwt_nbf_time
+    nbf = Time.now.to_i - JWT_NBF_TIME
 
     payload = { data: auth_hash, exp: exp, nbf: nbf }
     JWT.encode payload, secret_key, 'HS256'
